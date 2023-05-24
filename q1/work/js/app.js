011d39bbb3afc1628595ee57ee79a2628108531b
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-05-24 19:42:36 +0900

    q1-1完了

--- q1/work/js/app.js
+++ q1/work/js/app.js
@@ -1,1 +1,16 @@
-// branch作成
+// Q1-1
+// #q1の色変更
+window.onload = function () {
+  // ページの読み込み時に発生する関数
+  const q1 = document.getElementById("q1").style.color = "green";
+  // id"q1"の取得と文字の色の変更（関数の動作の中身）
+  console.log(q1);
+  // 関数の実行
+};
+
+// Q1-2
+// クリック時にボタンの色の変更
+/*document.getElementsByClassName("btn").onclick = function(){
+  document.getElementById("q2").style.background = "pink"
+};*/
+
