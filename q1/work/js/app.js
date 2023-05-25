cc68c563710a1acb24e5b1ccd2a6cca41ef2cdd1
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-05-25 12:17:22 +0900

    q1-3回答途中

--- q1/work/js/app.js
+++ q1/work/js/app.js
@@ -10,7 +10,34 @@ window.onload = function () {
 
 // Q1-2
 // クリック時にボタンの色の変更
-/*document.getElementsByClassName("btn").onclick = function(){
-  document.getElementById("q2").style.background = "pink"
-};*/
+document.getElementById('q2').addEventListener('click',
+  // html内でq2というidがクリックされたときに発生するイベント
+  function () {
+    // 発生するイベントの関数
+    this.style.backgroundColor = 'pink';
+    // q2の背景にpinkを指定
+  });
 
+// Q1-3
+// クリックしてフェードアウト
+/*
+const q3 = document.querySelector('#q3.btn');
+q3.addEventListener('click', ()=>{
+  fadeOut(q3);
+});
+
+function fadeOut(q3) {
+  let opacity =1;
+  const timer = setInterval(()=>{
+    if (opacity <= 0){
+      clearInterval(timer);
+      q3.style.display ='none';
+    }else {
+      q3.style.opacity = opacity;
+      opacity -= 0.1;
+    }
+  }, 3000);
+}*/
+  document.querySelector('#q3.btn').onclick(function(){
+    this.fadeOut(3000);
+  });
