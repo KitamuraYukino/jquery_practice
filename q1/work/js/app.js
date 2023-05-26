238f1be008b1856425913a0690a7cbab5d65b719
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-05-27 00:09:16 +0900

    q1,q2修正q5完了

--- q1/work/js/app.js
+++ q1/work/js/app.js
@@ -1,21 +1,23 @@
 // Q1-1
 // #q1の色変更
-window.onload = function () {
-  // ページの読み込み時に発生する関数
-  const q1 = document.getElementById("q1").style.color = "green";
-  // id"q1"の取得と文字の色の変更（関数の動作の中身）
-  console.log(q1);
-  // 関数の実行
-};
+$(function(){
+  // jQuery実行
+  $(document).ready(function(){
+    // ページ読み込み時に発生
+  $('#q1').css("color", "green");
+  // id#q1のcssにcolor/greenを追加
+});
+});
 
 // Q1-2
 // クリック時にボタンの色の変更
-document.getElementById('q2').addEventListener('click',
-  // html内でq2というidがクリックされたときに発生するイベント
-  function () {
-    // 発生するイベントの関数
-    this.style.backgroundColor = 'pink';
-    // q2の背景にpinkを指定
+  $(function(){
+    // jQuery実行
+    $("#q2").on('click', function(){
+      // id#q2を含む要素内がクリックされたときに発生する関数
+      $(this).css('background', 'pink');
+      // id#q2のCSSにbackground/pinkを追加
+    });
   });
 
 // Q1-3
@@ -30,7 +32,25 @@ $(function() {
 // クリックしてサイズ変更
 $(function(){
   $('#q4').on("click", function(){
-    $(this).width(250);
-    $(this).height(100);
+    $(this).width(300);
+    $(this).height(75);
+    $(this).css('font-size', '125%')
+  });
+});
+
+// Q1-5
+// クリックしてDOMの挿入
+$(function(){
+  // jQueryの実行
+  $('#q5').on('click', function(){
+    // id#q5を含む要素がクリックされたときに発生する
+    $(this).before('<span>DOMの前</span>');
+    // q5の要素の前にテキストの追加
+    $(this).prepend('DOMの中の前');
+    // q5の要素内で既存テキストの前にテキストを追加
+    $(this).append('DOMの中の後');
+    // q5の要素内で既存テキストの後にテキストを追加
+    $(this).after('<span>DOMの後</span>');
+    // q5の要素の後にテキストの追加
   });
 });
