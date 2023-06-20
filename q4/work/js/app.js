3e370303a4e21a829eaa5cd2bb3406a9d5a30fdf
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-06-20 18:07:28 +0900

    q4修正

--- q4/work/js/app.js
+++ q4/work/js/app.js
@@ -2,7 +2,7 @@
 // 同時にホームコンテンツにnav-hiddenを追加
 
 $(function () {
-  $('.nav-item').click(function () {
+  $('.nav-item').on('click', function () {
     let index = $(this).index();
 
     $('.description li').addClass('is-hidden');
