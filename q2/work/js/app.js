c2b28cbf88934cc70c2a11abed00b9afc9d6e83c
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-08-03 00:33:04 +0900

    q2修正完了

--- q2/work/js/app.js
+++ q2/work/js/app.js
@@ -1,13 +1,25 @@
-$(function(){
+$(function () {
   // jQuery実行
-  $('.btn').on('click', function(){
-  // class(btn)を含む要素をクリックしたときに発生
-    $('.modal_bg, .modal_win').fadeIn();
-  // class(modal_bg),(modal_win)を含む要素をフェードインさせる
+  let modalOpen = false;
+  //modalの開閉を管理するための変数を作成
+  $('.btn').on('click', function () {
+    // class(btn)を含む要素をクリックしたときに発生
+    if (!modalOpen) {
+      //modalOpenとanimationがともにfailseの場合
+      modalOpen = true;
+      //modalOpenをtrueに変更
+      $('.modal_bg, .modal_win').fadeIn();
+      // class(modal_bg),(modal_win)を含む要素をフェードインさせる
+    }
   });
-  $('.c-modal_close').on('click', function(){
-  // class(c-modal_close)を含む要素をクリックしたときに発生
-    $('.modal_bg, .modal_win').fadeOut();
-  // class(modal_bg),(modal_win)を含む要素をフェードアウトさせる
+  $('.c-modal_close').on('click', function () {
+    // class(c-modal_close)を含む要素をクリックしたときに発生
+    if (modalOpen) {
+      //modalOpenがtrue,animationがfalseの場合
+      $('.modal_bg, .modal_win').stop(true, false).fadeOut();
+      // class(modal_bg),(modal_win)を含む要素をフェードアウトさせる
+      modalOpen = false;
+      //modalOpenをfalseに変更
+    }
   });
 });
