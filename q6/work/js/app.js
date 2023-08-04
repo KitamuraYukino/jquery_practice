bcac4d30736326e29fdeb11abd07d82a7b28e537
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-08-04 14:41:59 +0900

    q6修正完了

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -4,19 +4,28 @@ $(function () {
   $('.select-box').change(function () {
     //変数selectedCategoryに.select-boxの値を代入
     const selectedCategory = $(this).val();
-//.food-list内のliに一致するものすべてに繰り返し処理
-    $('.food-list li').each(function () {
-    //li要素内のcategory-type属性の値を取得してcategoryTypeに代入
-      const categoryType = $(this).data('category-type');
-//selectedCategoryがallまたはcategoryTypeと一致する場合
-      if (selectedCategory === 'all' || selectedCategory === categoryType) {
-      //あてはまるものを表示
-        $(this).show();
-      //あてはまらない要素について
-      } else {
-      //隠す
-        $(this).hide();
-      }
-    });
+
+    //「全て」を選択した場合.food-listの値をすべて表示する
+    if (selectedCategory === 'all') {
+      //.food-listの値をすべて表示する
+      $('.food-list li').show();
+      //それ以外の場合
+    } else {
+
+      //.food-list内のliに一致するものすべてに繰り返し処理
+      $('.food-list li').each(function () {
+        //li要素内のcategory-type属性の値を取得してcategoryTypeに代入
+        const categoryType = $(this).data('category-type');
+        //selectedCategoryがallまたはcategoryTypeと一致する場合
+        if (selectedCategory === 'all' || selectedCategory === categoryType) {
+          //あてはまるものを表示
+          $(this).show();
+          //あてはまらない要素について
+        } else {
+          //隠す
+          $(this).hide();
+        }
+      });
+    }
   });
 });
