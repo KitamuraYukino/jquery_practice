6f5c56ac8b41ee3c519ba2daa0ed9b85afc85aec
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-08-04 14:14:14 +0900

    q5修正完了

--- q5/work/js/app.js
+++ q5/work/js/app.js
@@ -4,12 +4,12 @@
 $(function(){
   //.dropdown内のli要素のホバー時に発生するイベント
   $('.dropdwn > li').hover(function(){
-    //.dropdown内の.dropdown_menuを含む要素を見つけてスライドダウンさせる
-    $(this).find('.dropdwn_menu').slideDown();
+    //.dropdown内の.dropdown_menuを含む要素を見つけてスライドダウンさせる。途中でホバーが外れた場合はアニメーションを止める。
+    $(this).find('.dropdwn_menu').stop().slideDown();
   },
   //ホバーが外れた場合
   function(){
-    //.dropdown内の.dropdown_menuを含む要素をスライドアップさせる（元に戻す）
-    $(this).find('.dropdwn_menu').slideUp();
+    //.dropdown内の.dropdown_menuを含む要素をスライドアップさせる（元に戻す）途中でホバーが外れた場合はアニメーションを止める。
+    $(this).find('.dropdwn_menu').stop().slideUp();
   });
 });
