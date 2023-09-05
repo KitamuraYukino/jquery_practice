81bfd7bd4898f1288deb46f9fb5e21475b24725a
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-09-05 22:15:18 +0900

    三項演算子修正

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -59,24 +59,34 @@ $(function () {
 
 
         //結果を使ったループ処理
-        for (let i = len-1; i >= 0; i--) {
-          //著者の情報がundefindの場合は「作者（不明）」と表示
-          if (typeof result[0].items[i]["dc:creator"] == 'undefined') {
-            result[0].items[i]["dc:creator"] = '作者不明'
-          }
-          //出版社の情報がundefindの場合は「出版社（不明）」と表示
-          if (typeof result[0].items[i]["dc:publisher"] == 'undefined') {
-            result[0].items[i]["dc:publisher"] = "出版社不明"
-          }
+        for (let i = len - 1; i >= 0; i--) {
+
+          //変数[title]に結果の中のタイトルを代入
+          let title = result[0].items[i].title;
+          //変数[creator]に結果の中の著者を代入
+          let creator = result[0].items[i]["dc:creator"];
+          //変数[publisher]に結果の中の出版社を代入
+          let publisher = result[0].items[i]["dc:publisher"];
+          //変数[title]に結果の中のタイトルを代入
+          let imfo = result[0].items[i]["@id"];
+
+          //タイトルの情報がundefindの場合は「作者（不明）」と表示
+          title = typeof title !== 'undefined' ? title : 'タイトル不明';
+          //著者の情報がundefindの場合は「作者不明」と表示
+          creator = typeof creator !== 'undefined' ? creator : '作者不明';
+          //出版社の情報がundefindの場合は「出版社不明」と表示
+          publisher = typeof publisher !== 'undefined' ? publisher : '出版社不明';
+          //書籍情報の情報がundefindの場合は「書籍情報不明」と表示
+          imfo = typeof imfo !== 'undefined' ? imfo : '書籍情報不明';
 
           //htmlに結果を追加
           html += `
         <li class="lists-item">
         <div class="list-inner">
-        <p>タイトル：${result[0].items[i].title}</p>
-        <p>作者：${result[0].items[i]["dc:creator"]}</p>
-        <p>出版社：${result[0].items[i]["dc:publisher"]}</p>
-        <a href=${result[0].items[i]["@id"]} target="_blank">書籍情報</a>
+        <p>タイトル：${title}</p>
+        <p>作者：${creator}</p>
+        <p>出版社：${publisher}</p>
+        <a href=${imfo} target="_blank">書籍情報</a>
         </div>
         </li>
         `;
