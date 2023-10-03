320f64bba8d6fe668531e107a3f1563ef4853fa6
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-10-04 05:52:53 +0900

    コード完成

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -31,8 +31,6 @@ $(function () {
       method: "GET",
     };
 
-    //後で消す
-    //console.log(settings.url);
 
     //処理成功時にhtmlを追加するための変数doneHtmlを作成
     let doneHtml = '';
@@ -40,14 +38,24 @@ $(function () {
     //処理失敗時にhtmlを追加するための変数failHtmlを作成
     let failHtml = '';
 
-
-    //Ajaxを実行し設定情報（setting）を呼び出す
+    //ajaxの実行
     $.ajax(settings).done(function (response) {
+
+      //処理成功時の関数handleSuccess実行
+      handleSuccess(response);
+
+      //ajaxの実行にエラーが発生した場合
+    }).fail(function (err) {
+
+      //処理失敗時の関数handleFailure実行
+      handleFailure(err);
+    });
+
+    //handleSuccessの定義
+    function handleSuccess(response) {
+
       //返ってきた情報の中の@graphのみ
       const result = response['@graph'];
-
-      //後で消す
-      console.log(result);
 
       //返ってきた情報にresult[0].itemsを得られた場合
       if (result[0].items) {
@@ -81,7 +89,7 @@ $(function () {
           itemsId = typeof itemsId !== 'undefined' ? itemsId : '書籍情報不明';
 
 
-          //htmlに結果を追加
+          //doneHtmlに結果を追加
           doneHtml += `
         <li class="lists-item">
         <div class="list-inner">
@@ -97,35 +105,61 @@ $(function () {
         //class[lists]にhtmlを追加
         $('.lists').prepend(doneHtml);
 
+        //同じ言葉で検索した場合はpageCountを増やす
         pageCount++;
 
         //返ってきた情報にresult[0].itemsを得られなかった場合
       } else {
+
+        //doneHtmlに追加する文
         doneHtml += `
         <p class="message">検索結果が見つかりませんでした。<br>
         別のキーワードで検索して下さい。</p>
         `;
 
-        //一度class[lists]を空にして、htmlを追加
+        //一度class[lists]を空にして、doneHtmlを追加
         $('.lists').empty().prepend(doneHtml);
 
       }
+    };
 
-      //class[search-input]の値が空のまま検索ボタンが押された場合
-    }).fail(function (err) {
-      const errStatus = err.status;
-      console.log(errStatus);
+    //handleFailureの定義
+    function handleFailure(err) {
 
-      errStatus !== '0'
-        ? failHtml += `
+      //返ってきたerrのstatusが0の場合
+      if (errStatus === 0) {
+
+        //failHtmlに追加する文
+        failHtml += `
         <p class="message">正常に通信できませんでした。<br>
         インターネットの接続の確認をしてください。</p>
         `
-        : false;
 
-      //一度class[lists]を空にして、htmlを追加
+        //返ってきたerrのstatusが400の場合
+      } else if (errStatus === 400) {
+
+
+        //failHtmlに追加する文
+        failHtml += `
+        <p class="message">検索ワードが有効ではありません。<br>
+        1文字以上で検索してください。</p>
+        `
+
+        //返ってきたerrのstatusが0または400以外の場合
+      } else {
+
+
+        //failHtmlに追加する文
+        failHtml += `
+        <p class="message">予期せぬエラーが発生しました。<br>
+        再読み込みを行ってください。</p>
+        `
+      };
+
+      //一度class[lists]を空にして、failHtmlを追加
       $('.lists').empty().prepend(failHtml);
-    });
+    };
+
 
   });
 
@@ -145,11 +179,3 @@ $(function () {
     $('.lists').empty();
   });
 });
-
-// $.ajax(settings)
-// .done(handleSuccess);
-// .fail(handleFailure);
-
-//err
-//err_failed_400:検索欄を空欄のまま処理を行った
-//ERR_FAILED 301:Ajax実行時のURLにタイプミスがあった
